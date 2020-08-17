14278883ddca55133e0ff78a1053d7437206ec25
akshayDatacode <akshaywebllisto@gmail.com>
2020-08-18 02:18:08 +0530

    Updates

---
+++ precache-manifest.4733c9a20fa9a02554d438e7aa947e3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34f9fbc35105e44e2a27b5df949aab78",
+    "url": "/udaan/index.html"
+  },
+  {
+    "revision": "6ba60b65b6684554ed84",
+    "url": "/udaan/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "cf049512d607fc801ebc",
+    "url": "/udaan/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6ba60b65b6684554ed84",
+    "url": "/udaan/static/js/2.f439daf5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/udaan/static/js/2.f439daf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf049512d607fc801ebc",
+    "url": "/udaan/static/js/main.8dec7262.chunk.js"
+  },
+  {
+    "revision": "c2238224a3608d0714ae",
+    "url": "/udaan/static/js/runtime-main.28121465.js"
+  }
+]);
