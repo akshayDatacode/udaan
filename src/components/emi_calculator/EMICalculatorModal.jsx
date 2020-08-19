f17b1fba7c4361407a46f470db523a7ab288d8cb
miss-shree <dhanshreenirgude80@gmail.com>
2020-08-20 01:24:34 +0530

    +Dhanshree | changes in the calculated Emi UI

--- src/components/emi_calculator/EMICalculatorModal.jsx
+++ src/components/emi_calculator/EMICalculatorModal.jsx
@@ -1,32 +1,44 @@
 import React, { Component } from "react";
+import { Table } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 class EMICalculatorModal extends Component {
   state = {};
   render() {
     return (
       <>
-        <h1>Calculator</h1>
-        <ul>
-          <li>
-            <h2>EMI Amount: {this.props.emiAmount}</h2>
-          </li>
-          <li>
-            <h2>File Charge{this.props.fileCharge}</h2>
-          </li>
-          <li>
-            <h2>Stamp Charge{this.props.stampCharge}</h2>
-          </li>
-          <li>
-            <h2>
+       <div className="detail_content plan_box ">
+       <Table striped bordered hover responsive>
+              <tbody>
+                <tr>
+         
+            <th style={{backgroundColor:"blue", color:"white"}}>EMI Amount: </th>
+            <td style={{backgroundColor:"blue", color:"white"}}><input value={this.props.emiAmount}/></td>
+            </tr>
+            <tr>
+            <th >File Charge:</th>
+            <td><input value={this.props.fileCharge}/></td>
+            </tr>
+              <tr>
+            <th>Stamp Charge</th>
+            <td><input value={this.props.stampCharge}/></td>
+</tr>
+        <tr>
+            <th>
               DownPayment = {this.props.persentPayment} + StampCharges{" "}
               {this.props.stampCharge} + FileCharge{this.props.fileCharge}{" "}
-            </h2>
-            <h1>
+            </th>
+            <td>
+            <input value=
               {this.props.persentPayment +
                 this.props.stampCharge +
-                this.props.fileCharge}
-            </h1>
-          </li>
-        </ul>
+                this.props.fileCharge}/>
+            </td>
+        </tr>
+
+</tbody>
+</Table>
+        </div>
       </>
     );
   }
