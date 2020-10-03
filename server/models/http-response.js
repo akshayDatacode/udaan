02e54a7c0812c7a2b225633974ee57d40f90a4db
akshayDatacode <akshaywebllisto@gmail.com>
2020-10-03 14:35:34 +0530

    node files transfer to new app udaan-api

--- server/models/http-response.js
+++
@@ -1,8 +0,0 @@
-class HttpResponse {
-  constructor(message, code) {
-    this.message = message; // Add a "message" property
-    this.code = code; // Adds a "code" property
-  }
-}
-
-module.exports = HttpResponse;
