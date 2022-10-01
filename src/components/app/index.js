0385c44ff357abc3ebf2263073a456d8a9378548
miss-shree <dhanshreenirgude80@gmail.com>
2022-10-01 16:02:19 +0530

    improve table header design

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import AppRoutes from "./app_routes/AllRoutes";
-import AuthRoutes from "./app_routes/AuthRoutes" 
+import AuthRoutes from "./app_routes/AuthRoutes"
 import { connect } from "react-redux";
 import { setLoginUser } from "../user/actions";
 import IndexHeaderComponent from "../header_components/IndexHeaderComponent";
@@ -9,25 +9,32 @@ class App extends Component {
   state = {
     currentUser: null
   }
-  
+
   componentDidMount() {
-    
+
     const getDataFromLocalStorage = localStorage.getItem("userDetails");
     const parseDataFromJSON = JSON.parse(getDataFromLocalStorage);
     console.log("Local Storage Data", parseDataFromJSON);
-   // this.setState({ currentUser: parseDataFromJSON });
+    // this.setState({ currentUser: parseDataFromJSON });
 
-    this.props.setLoginUser(parseDataFromJSON)   
+    this.props.setLoginUser(parseDataFromJSON)
   }
-  
+
   render() {
     const { currentUser } = this.props;
 
     return (
-      <div className="container">
-        {currentUser !== null && <IndexHeaderComponent /> }
-        {currentUser === null ? <AuthRoutes /> : <AppRoutes />}
-      </div>
+      <>
+        {currentUser !== null && <IndexHeaderComponent />}
+        {
+          currentUser === null ?
+            <AuthRoutes />
+            :
+            <div className="mt-5">
+              <AppRoutes />
+            </div>
+        }
+      </>
     )
   }
 }
--- src/components/dashboard/components/Dashboard/Dashboard.jsx
+++ src/components/dashboard/components/Dashboard/Dashboard.jsx
@@ -5,13 +5,23 @@ class Dashboard extends Component {
   render() {
     return (
       <>
-        <h1>Dashboard</h1>
-        <Link to="/applicants_list">
-          <div className="btn btn-success">Applicants List</div>
-        </Link>
-        <Link to="/loan_applicants_list">
-          <div className="btn btn-warning">Loan Application List</div>
-        </Link>
+        <div className="row mx-0">
+          <div className="col-12 p-0">
+            <div className="p-4 border-bottom shadow">
+              <h1>Dashboard</h1>
+            </div>
+            <div className="d-flex justify-content-center align-items-center mt-5">
+              <div>
+                <Link to="/applicants_list">
+                  <div className="btn btn-success mx-3">Applicants List</div>
+                </Link>
+                <Link to="/loan_applicants_list">
+                  <div className="btn btn-warning mx-3">Loan Application List</div>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
